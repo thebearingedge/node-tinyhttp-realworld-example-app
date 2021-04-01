6a4b04b9bcbb9135ad9a6fbb5d5350e81d4f9e75
Tim Davis <contact@timdav.is>
2021-03-31 21:09:13 -0700

    Make a sane suite setup helper.

--- api/auth/auth-router.test.js
+++ api/auth/auth-router.test.js
@@ -1,20 +1,7 @@
-import prisma from '@prisma/client'
-import { makeFetch } from 'supertest-fetch'
-import { createApi } from '../create-api.js'
 import { suite } from '../util/test-suite.js'
 
-const app = createApi()
-const server = app.listen()
-const client = makeFetch(server)
-const db = new prisma.PrismaClient()
-
-suite('auth router', test => {
-  test.after(async () => {
-    await db.$disconnect()
-    server.close()
-  })
-
-  test('not found', async () => {
+suite('auth: POST /api/users', ({ test, db, client }) => {
+  test('requires an email and password', async () => {
     await client('/api/fake').expect(404, {
       error: 'cannot GET /api/fake'
     })
--- api/util/test-suite.js
+++ api/util/test-suite.js
@@ -1,11 +1,34 @@
+import prisma from '@prisma/client'
 import { suite as uvuSuite } from 'uvu'
+import { makeFetch } from 'supertest-fetch'
+import { createApi } from '../create-api.js'
+
+const db = new prisma.PrismaClient()
+const app = createApi()
+const server = app.listen()
+const client = makeFetch(server)
 
 export const suite = (name, context, register) => {
   if (typeof register === 'undefined') {
     register = context
     context = undefined
   }
+
   const test = uvuSuite(name, context)
-  register(test)
+
+  test.after(async () => {
+    server.close()
+    await db.$disconnect()
+  })
+
+  test.before.each(async () => {
+    const deletions = Object.keys(db)
+      .filter(key => !key.startsWith('_'))
+      .map(entity => db[entity].deleteMany())
+    await db.$transaction(deletions)
+  })
+
+  register({ test, db, client })
+
   test.run()
 }
